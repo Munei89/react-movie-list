bc63e1c2c44f5c8f9d116af2f09a92705fdc795b
Munei Nengwenani <mnengwenani@qdb.qa>
2021-03-15 10:07:43 +0200

    Adding packages

---
+++ src/setupTests.ts
@@ -0,0 +1,25 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
+
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import 'jest-styled-components';
+
+// https://github.com/ant-design/ant-design/issues/21096
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
