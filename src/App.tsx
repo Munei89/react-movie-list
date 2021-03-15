40a487096874a2c32d7f3e914d3b0ce32d269e56
Munei Nengwenani <mnengwenani@qdb.qa>
2021-03-15 09:13:25 +0200

    Adding basic app files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "antd/dist/antd.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <Router>
+      <>
+          Here
+      </>
+
+    </Router>
+  );
+};
+
+export default App;
